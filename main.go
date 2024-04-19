8abed230d80f900ed0a12946fd14329c67273ee9
Ilya AGL <agibalovilya72@gmail.com>
2024-04-19 10:47:05 +0300

    Almost final version

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net/url"
 	"os"
 	"sync"
+	"io"
+	"log"
 
 	"github.com/gocolly/colly"
 )
@@ -71,13 +73,62 @@ func main() {
 
 		scrap(&imgs, source)
 		
-		fmt.Println(imgs)
+		//fmt.Println(imgs)
 
 		tpltAnother.Execute(w, nil)
 	})
 
 	http.HandleFunc("/get-button", func(w http.ResponseWriter, r *http.Request) {
-		
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		fmt.Println("HIIIII")
+
+		// err1 := os.RemoveAll("/images")
+		// if err1 != nil {
+		// 	log.Fatal("Cannot delete this folder :(")
+		// }
+
+		err := os.Mkdir("images", 0755)
+
+		if err != nil {
+			log.Fatal("Cannot find this path for folder :(")
+		}
+
+		for ind, img_path := range imgs {
+			var name string
+			if string(img_path[len(img_path) - 4]) == "." {
+				name = fmt.Sprintf("imgoo_%d%s", ind, img_path[len(img_path) - 4:])
+			} else {
+				name = fmt.Sprintf("imgoo_%d.jpg", ind)
+			}
+			file, err := os.Create(fmt.Sprintf("images/%s", name))
+			if err != nil {
+				log.Fatal("Cannot create this file :(")
+			}
+
+			client := http.Client{
+				CheckRedirect: func(r *http.Request, via []*http.Request) error {
+					r.URL.Opaque = r.URL.Path
+					return nil
+				},
+			}
+			
+			resp, err := client.Get(img_path)
+			if err != nil {
+				log.Fatal("Cannot get this client :(")
+			}
+			defer resp.Body.Close()
+
+			size, err := io.Copy(file, resp.Body)
+			if err != nil {
+				log.Fatal("Cannot copy size :(")
+			}
+ 
+    		defer file.Close()
+ 
+    		fmt.Printf("Downloaded a file %s with size %d\n", name, size)
+			fmt.Fprintf(w, "<strong>Images downloaded</strong><br>")
+
+		}
 	})
 	http.ListenAndServe(":8080", nil)
 }
